5ebe7d7ecbd68d5e16e283d9728fb675a305827c
Keith Jerosky <kjerosky@gmail.com>
2026-02-04 22:12:34 -0700

    Implement camera with background (section 4)

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,55 @@
 mod vec3;
+mod ray;
 
 use crate::vec3::Vec3;
+use crate::ray::Ray;
+
+fn ray_color(ray: &Ray) -> Vec3 {
+    let unit_direction = ray.direction.normalized();
+    let a = (unit_direction.y + 1.0) / 2.0;
+
+    // Linear interpolation
+    Vec3::new(1.0, 1.0, 1.0) * (1.0 - a) + Vec3::new(0.5, 0.7, 1.0) * a
+}
+
+// ---------------------------------------------------------------------------
 
 fn main() {
-    const IMAGE_WIDTH: usize = 256;
-    const IMAGE_HEIGHT: usize = 256;
+    let ideal_aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+
+    let image_height = (image_width as f64 / ideal_aspect_ratio) as i32;
+    let image_height = i32::max(image_height, 1);
+
+    let focal_length = 1.0;
+    let viewport_height = 2.0;
+    let viewport_width = viewport_height * image_width as f64 / image_height as f64;
+    let camera_center = Vec3::new(0.0, 0.0, 0.0);
+
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+    let pixel_delta_u = viewport_u / image_width as f64;
+    let pixel_delta_v = viewport_v / image_height as f64;
+
+    let viewport_upper_left = camera_center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+    let pixel_00_location = viewport_upper_left + (pixel_delta_u + pixel_delta_v) / 2.0;
 
     println!("P3");
-    println!("{IMAGE_WIDTH} {IMAGE_HEIGHT}");
+    println!("{image_width} {image_height}");
     println!("255");
 
-    for j in 0..IMAGE_HEIGHT {
-        eprintln!("Scanlines remaining: {}", IMAGE_HEIGHT - j);
-        for i in 0..IMAGE_WIDTH {
-            let color = Vec3::new(
-                i as f64 / (IMAGE_WIDTH - 1) as f64,
-                j as f64 / (IMAGE_HEIGHT - 1) as f64,
-                0.0
-            );
+    for j in 0..image_height {
+        eprintln!("Scanlines remaining: {}", image_height - j);
+        for i in 0..image_width {
+            let pixel_center = pixel_00_location + (pixel_delta_u * i as f64) + (pixel_delta_v * j as f64);
+            let ray_direction = pixel_center - camera_center;
+            let ray = Ray {
+                origin: camera_center,
+                direction: ray_direction,
+            };
+
+            let color = ray_color(&ray);
 
             color.println();
         }
--- src/vec3.rs
+++ src/vec3.rs
@@ -1,12 +1,87 @@
+use std::ops::{Add, Div, Mul, Sub};
+
+#[derive(Copy, Clone)]
 pub struct Vec3 {
-    x: f64,
-    y: f64,
-    z: f64,
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
 }
+
+// ---------------------------------------------------------------------------
+
+impl Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+// ---------------------------------------------------------------------------
+
+impl Sub for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
+
+// ---------------------------------------------------------------------------
+
+impl Mul<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Self {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        }
+    }
+}
+
+// ---------------------------------------------------------------------------
+
+impl Div<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        self * (1.0 / rhs)
+    }
+}
+
+// ---------------------------------------------------------------------------
 
 impl Vec3 {
     pub fn new(x: f64, y: f64, z: f64) -> Self {
         Self { x, y, z }
+    }
+
+    // -----------------------------------------------------------------------
+
+    pub fn length_squared(&self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    // -----------------------------------------------------------------------
+
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    // -----------------------------------------------------------------------
+
+    pub fn normalized(&self) -> Self {
+        *self / self.length()
     }
 
     // -----------------------------------------------------------------------
